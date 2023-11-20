f4df94372d0f68660ffafa900246260b1a3d756b
Jonas Barkå <jonas.barka@gmail.com>
2023-11-20 21:19:16 +0100

    Add explicit cache version to service worker

--- src/LastCoupleOut/wwwroot/service-worker.published.js
+++ src/LastCoupleOut/wwwroot/service-worker.published.js
@@ -7,7 +7,8 @@ self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 const cacheNamePrefix = 'offline-cache-';
-const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
+const cacheVersion = 0.1
+const cacheName = `${cacheNamePrefix}${cacheVersion}-${self.assetsManifest.version}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
 const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
