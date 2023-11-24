e2bdd398f4f1131fe5a674a2a35cd738330cbc43
Jonas Barkå <jonas.barka@gmail.com>
2023-11-24 20:24:36 +0100

    Add first working version of couples page

--- src/Infrastructure/Dto/CoupleDto.cs
+++ src/Infrastructure/Dto/CoupleDto.cs
@@ -2,11 +2,15 @@
 
 public record CoupleDto
 {
-    public required int TotalScore { get; init; }
+    public required int CoupleScore { get; init; }
+
+    public required int FirstAttendeeId { get; init; }
 
     public required string FirstAttendeeName { get; init; }
 
     public required string FirstAttendeeScores { get; init; }
+
+    public required int SecondAttendeeId { get; init; }
 
     public required string SecondAttendeeName { get; init; }
 
