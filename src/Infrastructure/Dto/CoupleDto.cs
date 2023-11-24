a75eb4527631c3e723b032b27979522ea1236381
Jonas Barkå <jonas.barka@gmail.com>
2023-11-24 16:29:10 +0100

    Add first couples page with placeholder data

---
+++ src/Infrastructure/Dto/CoupleDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.Dto;
+
+public record CoupleDto
+{
+    public required int TotalScore { get; init; }
+
+    public required string FirstAttendeeName { get; init; }
+
+    public required string FirstAttendeeScores { get; init; }
+
+    public required string SecondAttendeeName { get; init; }
+
+    public required string SecondAttendeeScores { get; init; }
+}
